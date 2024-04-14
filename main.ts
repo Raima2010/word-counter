5ac77d93986db5639dd1c3a629b732751023abe6
Raima2010 <164664818+Raima2010@users.noreply.github.com>
2024-04-14 18:41:55 +0500

    created a cli based word counter

---
+++ main.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+
+const answers: {
+    sentence:string
+} = await inquirer.prompt(
+ 
+    [
+        {
+          name:"sentence",
+          type:"input",
+          message:"enter your sentence to count the words: "
+
+         }
+    ]
+) 
+const words = answers.sentence.trim().split(" ")
+   console.log(`your sentence word count is ${words.length}`)
